39296dd5ba919ba99ac6291de2743b855d88998b
unknown <you@example.com>
2019-12-16 09:13:39 -0800

    'jumbled-word-version-2'

--- jumbleword/models.py
+++ jumbleword/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Word(models.Model):
     word = models.CharField(max_length=50)
 
-    def __str(self):
-        return '%s' %( self.word )
+    def __str__(self):
+        return '%s' %(self.word)
 
 
 
